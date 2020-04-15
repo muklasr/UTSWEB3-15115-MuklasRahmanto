e4221a5c172ed83fe150da7fef63ef4c39c23b36
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-15 14:59:12 +0700

    bismillah

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} 
+from "react-router-dom";
+import Header from "./Header";
+import Blog from "./Blog";
+import BlogDetail from "./BlogDetail";
+
+//install bootstrap dulu
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Ini merupakan functional component
+// https://getbootstrap.com/docs/4.4/examples/navbar-static/
+class App extends React.Component {
+  render() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/about">
+            About
+          </Route>
+          <Route path="/blog/:articleId" component={BlogDetail} />
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/">
+            Home
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+}
+export default App;
